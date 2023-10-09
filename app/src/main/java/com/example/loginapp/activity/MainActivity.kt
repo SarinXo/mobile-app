48e86b91f38e6c85aedeab32b2d745af0c45ee49
rollo <rollovitch.roll@yandex.ru>
2023-10-10 01:51:11 +0300

    refactor

--- app/src/main/java/com/example/loginapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/activity/MainActivity.kt
@@ -1,20 +1,24 @@
-package com.example.loginapp
+package com.example.loginapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.databinding.adapters.ActionMenuViewBindingAdapter
+import com.example.loginapp.R
+import com.example.loginapp.databinding.ActivityLoginBinding
+
 class MainActivity : AppCompatActivity() {
 
     lateinit var username : EditText
     lateinit var password: EditText
     lateinit var loginButton: Button
+
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timetable)
-/*        var button: Button=findViewById(R.id.loginButton)
-        button.setOnClickListener {
-            *//*startActivities(Intent(this, ))*//*
-        }*/
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/example/loginapp/activity/TimetableActivity.kt
+++ app/src/main/java/com/example/loginapp/activity/TimetableActivity.kt
@@ -1,4 +1,4 @@
-package com.example.loginapp
+package com.example.loginapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 
