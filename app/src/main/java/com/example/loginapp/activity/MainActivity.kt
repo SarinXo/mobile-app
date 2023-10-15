40ccbc87197383ea1700c8c80137e0009e2d1814
rollo <rollovitch.roll@yandex.ru>
2023-10-15 21:32:25 +0300

    + in work

--- app/src/main/java/com/example/loginapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/activity/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.databinding.adapters.ActionMenuViewBindingAdapter
 import com.example.loginapp.R
 import com.example.loginapp.databinding.ActivityLoginBinding
 
@@ -17,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
